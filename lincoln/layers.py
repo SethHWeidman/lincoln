bed023928cff3851e0864f007f352724e2f5f52f
Seth Weidman <sethhweidman@gmail.com>
2018-09-14 11:10:10 -0700

    Clean up Convolutions notebook, add convolution layer

--- lincoln/layers.py
+++ lincoln/layers.py
@@ -4,7 +4,7 @@ from typing import List, Tuple, Dict
 import torch
 from torch import Tensor
 
-from .operations import Operation, ParamOperation, WeightMultiply, BiasAdd
+from .operations import Operation, ParamOperation, WeightMultiply, BiasAdd, Conv2D
 from .activations import Activation, LinearAct
 from .exc import MatchError, DimensionError
 from .utils import assert_same_shape
@@ -12,7 +12,7 @@ from .utils import assert_same_shape
 
 class Layer(object):
 
-    def __init__(self, 
+    def __init__(self,
                  neurons: int) -> None:
         self.neurons = neurons
         self.first = True
@@ -22,50 +22,50 @@ class Layer(object):
 
     def _setup_layer(self, num_in: int) -> None:
         pass
-        
+
     def forward(self, input_: Tensor) -> Tensor:
         if self.first:
             self._setup_layer(input_.shape[-1])
-            self.first = False            
+            self.first = False
         self.input_ = input_
-        
+
         for operation in self.operations:
 
             input_ = operation.forward(input_)
-            
+
         self.output = input_
 
         return self.output
 
     def backward(self, output_grad: Tensor) -> Tensor:
-        
+
         assert_same_shape(self.output, output_grad)
-        
+
         for operation in self.operations[::-1]:
             output_grad = operation.backward(output_grad)
-            
+
         input_grad = output_grad
-        
-        assert_same_shape(self.input_, input_grad)        
-        
+
+        assert_same_shape(self.input_, input_grad)
+
         self._param_grads()
-        
+
         return input_grad
-      
+
     def _param_grads(self) -> Tensor:
-        
+
         self.param_grads = []
         for operation in self.operations:
             if issubclass(operation.__class__, ParamOperation):
-                self.param_grads.append(operation.param_grad)      
+                self.param_grads.append(operation.param_grad)
 
 class Dense(Layer):
     '''
-    Once we define all the Operations and the outline of a layer, all that remains to implement here 
+    Once we define all the Operations and the outline of a layer, all that remains to implement here
     is the _setup_layer function!
     '''
-    def __init__(self, 
-                 neurons: int, 
+    def __init__(self,
+                 neurons: int,
                  activation: Activation = LinearAct) -> None:
         super().__init__(neurons)
         self.activation = activation
@@ -73,11 +73,34 @@ class Dense(Layer):
     def _setup_layer(self, num_in: int) -> None:
         # weights
         self.params.append(torch.empty(num_in, self.neurons).uniform_(-1, 1))
-        
+
         # bias
         self.params.append(torch.empty(1, self.neurons).uniform_(-1, 1))
-        
-        self.operations = [WeightMultiply(self.params[0]), 
+
+        self.operations = [WeightMultiply(self.params[0]),
                            BiasAdd(self.params[1])] + [self.activation]
 
-        
+
+class Conv2D(Layer):
+    '''
+    Once we define all the Operations and the outline of a layer, all that remains to implement here
+    is the _setup_layer function!
+    '''
+    def __init__(self,
+                 param_size: int,
+                 out_channels: int,
+                 activation: Activation = LinearAct) -> None:
+        super().__init__()
+        self.param_size = param_size
+        self.out_channels = out_channels
+        self.activation = activation
+
+
+    def _setup_layer(self, num_in: int) -> None:
+        # weights
+        conv_param = torch.empty(num_in,
+                                 self.param_size,
+                                 self.param_size,
+                                 self.out_channels).uniform_(-1, 1)
+
+        self.operations = [Conv2D(conv_param)] + [self.activation]
--- lincoln/operations.py
+++ lincoln/operations.py
@@ -1,7 +1,7 @@
 import torch
 from torch import Tensor
 
-from .utils import assert_same_shape
+from .utils import assert_same_shape, assert_dim
 
 class Operation(object):
 
@@ -91,3 +91,131 @@ class BiasAdd(ParamOperation):
     def _param_grad(self, output_grad: Tensor) -> Tensor:
         param_grad = torch.ones_like(self.param) * output_grad
         return torch.sum(param_grad, dim=0).reshape(1, param_grad.shape[1])
+
+
+class Conv2D(ParamOperation):
+
+
+    def __init__(self, param: Tensor):
+        assert_dim(param, 4)
+        super().__init__(param)
+        self.param_size = self.param.shape[0]
+        self.param_pad = self.param_size // 2
+        self.in_channels = self.param.shape[2]
+        self.out_channels = self.param.shape[3]
+
+
+    def _pad_1d_obs(self, obs: Tensor) -> Tensor:
+        assert_dim(obs, 1)
+        z = torch.Tensor([0])
+        z = z.repeat(self.param_pad)
+        return torch.cat([z, obs, z])
+
+
+    def _pad_1d_batch(self, inp: Tensor) -> Tensor:
+        assert_dim(inp, 2)
+        outs = [self._pad_1d_obs(obs) for obs in inp]
+        return torch.stack(outs)
+
+
+    def _pad_2d_obs(self, inp: Tensor):
+        assert_dim(inp, 2)
+        inp_pad = self._pad_1d_batch(inp)
+        other = torch.zeros(self.param_pad, inp.shape[0] + self.param_pad * 2)
+        return torch.cat([other, inp_pad, other])
+
+
+    def _pad_2d_batch(self, inp: Tensor):
+        assert_dim(inp, 3)
+        outs = [self._pad_2d_obs(obs) for obs in inp]
+        return torch.stack(outs)
+
+
+    def _select_channel(inp: Tensor, i: int):
+        return torch.index_select(inp, dim=2, index=torch.LongTensor([i])).squeeze(2)
+
+
+    def _pad_2d_channel(inp: Tensor, num: int):
+        '''
+        "inp" is a 3 dimensional tensor:
+        * image width
+        * image height
+        * channels
+        '''
+        assert_dim(inp, 3)
+        num_channels = inp.shape[2]
+        return torch.stack([_pad_2d_obs(self._select_channel(inp, i), num)
+                            for i in range(num_channels)], dim=2)
+
+
+    def _pad_conv_input(self):
+        return torch.stack([_pad_2d_channel(obs, self.param_mid)
+                            for obs in self.input_])
+
+
+    def _compute_output_obs(self, obs: Tensor):
+        assert_dim(obs, 3)
+        obs_pad = self._pad_2d_channel(obs)
+
+        out = torch.zeros(obs.shape[:2] + (self.out_channels,))
+        for c_out in range(self.out_channels):
+            for c_in in range(self.in_channels):
+                for o_w in range(out.shape[0]):
+                    for o_h in range(out.shape[1]):
+                        for p_w in range(self.param_size):
+                            for p_h in range(self.param_size):
+                                out[o_w][o_h][c_out] += \
+                                fil[p_w][p_h][c_in][c_out] * obs_pad[o_w+p_w][o_h+p_h][c_in]
+        return out
+
+
+    def _output(self):
+
+        outs = [self._compute_output_obs(obs) for obs in self.input_]
+        return torch.stack(outs)
+
+
+    def _compute_grads_obs(self, input_obs: Tensor,
+                           output_grad_obs: Tensor) -> Tensor:
+        assert_dim(input_obs, 3)
+        assert_dim(output_grad_obs, 3)
+        output_obs_pad = self._pad_2d_channel(output_grad_obs)
+        input_grad = torch.zeros_like(input_obs)
+
+        for c_in in range(self.in_channels):
+            for c_out in range(self.out_channels):
+                for i_w in range(input_obs.shape[0]):
+                    for i_h in range(input_obs.shape[1]):
+                        for p_w in range(self.param_size):
+                            for p_h in range(self.param_size):
+                                input_grad[i_w][i_h][c_in] += \
+                                output_obs_pad[i_w+self.param_size-p_w-1][i_h+self.param_size-p_h-1][c_out] \
+                                * fil[p_w][p_h][c_in][c_out]
+
+        return input_grad
+
+
+    def _input_grad(self, output_grad: Tensor) -> Tensor:
+
+        grads = [self._compute_grads_obs(self.input_[i], output_grad[i]) for i in range(output_grad.shape[0])]
+
+        return torch.stack(grads)
+
+
+    def _param_grad(self, output_grad: Tensor) -> Tensor:
+
+        inp_pad = self._pad_conv_input(self.input_)
+
+        param_grad = torch.zeros_like(self.param)
+
+        for i in range(self.input_.shape[0]):
+            for c_in in range(in_channels):
+                for c_out in range(out_channels):
+                    for o_w in range(output_grad[1]):
+                        for o_h in range(output_grad[2]):
+                            for p_w in range(self.param_size):
+                                for p_h in range(self.param_size):
+                                    param_grad[p_w][p_h][c_in][c_out] += \
+                                    inp_pad[i][o_w+p_w][o_h+p_h][c_in] \
+                                    * output_grad[i][o_w][o_h][c_out]
+        return param_grad
