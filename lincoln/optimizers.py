8cc0ce213d8c2221b6180ebcb9258d8a9eac9a61
Seth Weidman <sethhweidman@gmail.com>
2018-09-14 21:54:35 -0700

    Conv does NOT work

--- lincoln/optimizers.py
+++ lincoln/optimizers.py
@@ -6,21 +6,21 @@ from .layers import Layer
 class Optimizer(object):
     def __init__(self):
         pass
-        
+
     def step(self, layer: Layer) -> None:
         assert len(layer.params) == len(layer.param_grads)
         for param, grad in zip(layer.params, layer.param_grads):
             self._update_rule(param, grad)
-            
+
     def _update_rule(self, param: Tensor, grad: Tensor):
         raise NotImplementedError()
 
 
 class SGD(Optimizer):
-    def __init__(self,  
+    def __init__(self,
                  lr: float = 0.003) -> None:
         super().__init__()
         self.lr = lr
-        
+
     def _update_rule(self, param: Tensor, grad: Tensor) -> None:
         param.sub_(self.lr*grad)
